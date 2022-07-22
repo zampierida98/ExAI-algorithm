02b7dd8e24cfd6078a324b7685b2bb2324f7b8c8
zampierida98 <zampierida98@gmail.com>
2022-07-22 12:58:50 +0200

    comparazione con MLP, sistemati dataset, provato kr-vs-kp con sup rel

---
+++ comparation/SCRIPT_MLP.py
@@ -0,0 +1,67 @@
+"""
+SCRIPT CHE TESTA MLP (Multi Layer Perceptron)
+Codice da: https://machinelearninggeek.com/multi-layer-perceptron-neural-network-using-python/
+"""
+
+# IMPORT
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import precision_score, recall_score, accuracy_score
+
+# VARIABLES
+DATASET_PATH = "../dataset/cell_samples.csv"
+CLASS_COLUMN = "Class"
+MISSING = "?"
+POS_CLASS = 2
+NEG_CLASS = 4
+
+""" DATASET_PATH = "../dataset/Balloons/adult+stretch.data"
+CLASS_COLUMN = "inflated"
+MISSING = "?"
+POS_CLASS = 0  # dopo il Label Encoding
+NEG_CLASS = 1  # dopo il Label Encoding """
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(DATASET_PATH)
+    df = df.replace({MISSING:np.NaN})
+    
+    # drop columns with only missing values
+    df = df.dropna(axis=1, how='all')
+
+    # drop rows with missing values
+    df = df.dropna()
+
+    # Creating label encoder
+    le = preprocessing.LabelEncoder()
+
+    # Converting string labels into numbers
+    for column in list(df.columns):
+        if df[column].dtype == "object":
+            df[column] = le.fit_transform(df[column])
+
+    # Split the dataset
+    X = df.drop(columns=CLASS_COLUMN)
+    y = df[CLASS_COLUMN]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y)
+    print(len(X_train), len(X_test), len(y_train), len(y_test))
+
+    # Create model object
+    clf = MLPClassifier(hidden_layer_sizes=(6,5),
+                        random_state=5,
+                        verbose=True,
+                        learning_rate_init=0.01)
+
+    # Fit data onto the model
+    clf.fit(X_train, y_train)
+
+    # Make prediction on test dataset
+    ypred = clf.predict(X_test)
+
+    # Evaluate the Model
+    print("precision:", precision_score(y_test, ypred, pos_label=POS_CLASS))
+    print("recall:", recall_score(y_test, ypred, pos_label=POS_CLASS))
+    print("accuracy:", accuracy_score(y_test, ypred))
